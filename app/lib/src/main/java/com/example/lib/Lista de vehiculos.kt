5391b2abdefe0410783d6b8091a906a39fedd014
JoseDonato616 <jdg6100017@lasallebajio.edu.mx>
2026-03-04 23:23:54 -0600

    Lista de vehiculos

---
+++ app/lib/src/main/java/com/example/lib/Lista de vehiculos.kt
@@ -0,0 +1,39 @@
+package com.example.lib
+
+abstract class Vehiculo(val marca: String, val velocidadMaxima: Int) {
+
+    abstract fun tipoVehiculo(): String
+
+    fun mostrarInfo() {
+        println("${tipoVehiculo()}: $marca – Vel. max: $velocidadMaxima km/h")
+    }
+}
+
+class Auto(marca: String, velMax: Int) : Vehiculo(marca, velMax) {
+    override fun tipoVehiculo() = "Auto"
+}
+
+// TODO: Crea Moto y Camion de forma similar
+
+class Moto(marca: String, velMax: Int) : Vehiculo(marca, velMax) {
+    override fun tipoVehiculo() = "Moto"
+}
+
+class Camion(marca: String, velMax: Int) : Vehiculo(marca, velMax) {
+    override fun tipoVehiculo() = "Camion"
+}
+
+fun main() {
+
+    val vehiculos: List<Vehiculo> = listOf(
+        Auto("Toyota", 180),
+        Moto("Honda", 220),
+        Camion("Volvo", 120)
+    )
+
+    // TODO: Recorre la lista y llama mostrarInfo() en cada vehículo
+
+    for (vehiculo in vehiculos) {
+        vehiculo.mostrarInfo()
+    }
+}
